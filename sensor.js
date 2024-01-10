9ce0fb536c76c2ff184ae84ebdf9365e2054e336
Emmanuel Valentin <ema_110@outlook.es>
2024-01-10 17:37:19 -0600

    Aplicacion terminada

--- sensor.js
+++ sensor.js
@@ -10,24 +10,16 @@ configurationForm.addEventListener('submit', (event) => {
 
   const formData = new FormData(configurationForm);
   const pathPortA = formData.get('port-a');
-  const pathPortB = formData.get('port-b');
   const baudRate = formData.get('baud-rate');
 
-  const readerPort = new SerialPort({
+  const port = new SerialPort({
     path: pathPortA,
     baudRate: Number.parseInt(baudRate),
     autoOpen: false,
     lock: false,
   });
 
-  const writerPort = new SerialPort({
-    path: pathPortB,
-    baudRate: Number.parseInt(baudRate),
-    autoOpen: false,
-    lock: false,
-  });
-
-  initializePort(writerPort, readerPort);
+  initializePort(port);
   render(/* isPortConfigured = */ true);
 });
 
@@ -49,23 +41,21 @@ const render = (isPortConfigured = false) => {
   });
 };
 
-const initializePort = (readerPort, writerPort) => {
-  readerPort.pipe(new ReadlineParser());
+const initializePort = (port) => {
+  port.pipe(new ReadlineParser());
 
-  readerPort.open((err) => {
+  port.open((err) => {
     if (err) {
       render(/* isPortConfigured = */ false);
       throw new Error(`Error al abrir el puerto serial: ${err.message}`);
     }
-    console.log(`Port ${readerPort.path} connected`);
+    console.log(`Port ${port.path} connected`);
   });
 
-  readerPort.on('data', (data) => {
+  port.on('data', (data) => {
     const [receivedData] = data;
 
-    // Binary combination sent by the microcontroller
-    // const receivedDataToDecimal = Number.parseInt(receivedData);
-    const receivedDataToDecimal = Number.parseInt(data);
+    const receivedDataToDecimal = Number.parseInt(receivedData);
     const resolutionVoltage = 0.019607;
     const outputVoltage = receivedDataToDecimal * resolutionVoltage;
     const sensorVoltage = outputVoltage / 1.5151;
@@ -75,24 +65,6 @@ const initializePort = (readerPort, writerPort) => {
     humidityContainer.style = `background: linear-gradient(-45deg, var(--primary-200) ${humidity.toFixed(
       2
     )}%, var(--bg-300) 0%);`;
-  });
-
-  writerPort.open((err) => {
-    if (err) {
-      throw new Error(`Error al abrir el puerto serial: ${err.message}`);
-    }
-
-    let count = 0b00000000;
-
-    setInterval(() => {
-      if (count === 0b11111111) {
-        count = 0b00000000;
-      }
-
-      count += 0b00000001;
-      console.log(count.toString());
-      readerPort.write(count.toString());
-    }, 100);
   });
 };
 
